5c2eecb2f35f6c8a9c8d1d49c1693046d6949b8a
unknown <mdmehedyh55@gamil.com>
2022-02-28 22:36:16 +0600

    remove doctors-portal

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
-const serviceAccount = require("./n-doctors-portal-firebase-adminsdk.json");
+const serviceAccount = require("./doctors-portal-firebase-adminsdk.json");
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
