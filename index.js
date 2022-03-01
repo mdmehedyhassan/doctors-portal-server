b174d85ab55498a6aaaf2b7fd9135ade263999e7
unknown <mdmehedyh55@gamil.com>
2022-03-01 11:44:51 +0600

    73-5-1

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
-const serviceAccount = require("./doctors-portal-firebase-adminsdk.json");
+// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+const serviceAccount = require('./doctors-portal-firebase-adminsdk.json');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
